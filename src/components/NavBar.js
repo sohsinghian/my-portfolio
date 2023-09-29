1fc8dff6ff2dd5cbd1f98136863da376da7c0915
sohsinghian <sohsinghian@gmail.com>
2023-09-29 18:13:47 +0800

    update NavBar for mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavHashLink } from "react-router-hash-link";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
   const listStyle = "p-5 sm:p-2 hover:bg-slate-200 hover:text-black";
+  const mobileListStyle = "p-3 bg-blue-100 text-black hover:bg-white";
+
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const handleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
 
   return (
     <>
       <header>
         <nav class="fixed z-[100] w-full leading-4 bg-blue-950 text-white font-bold shadow-xl opacity-90">
-          <ul class="flex flex-row sm:text-sm sm:justify-between">
+          <div onClick={handleMobileNav}>
+            <AiOutlineMenu size={20} class="m-3 hidden sm:flex" />
+          </div>
+          <ul class="flex flex-row sm:text-sm sm:justify-between sm:hidden">
             <li class={listStyle}>
               <NavHashLink to="/#" smooth>
                 Home
@@ -35,6 +46,29 @@ const NavBar = () => {
               </NavHashLink>
             </li>
           </ul>
+          {mobileNav === true && (
+            <ul class="flex flex-col sm:text-sm sm:justify-between">
+              <NavHashLink to="/#" smooth onClick={handleMobileNav}>
+                <li class={mobileListStyle}>Home</li>
+              </NavHashLink>
+
+              <NavHashLink to="/#about" smooth onClick={handleMobileNav}>
+                <li class={mobileListStyle}>About</li>
+              </NavHashLink>
+
+              <NavHashLink to="/#skills" smooth onClick={handleMobileNav}>
+                <li class={mobileListStyle}>Skills</li>
+              </NavHashLink>
+
+              <NavHashLink to="/#projects" smooth onClick={handleMobileNav}>
+                <li class={mobileListStyle}>Projects</li>
+              </NavHashLink>
+
+              <NavHashLink to="/#contact" smooth onClick={handleMobileNav}>
+                <li class={mobileListStyle}>Contact</li>
+              </NavHashLink>
+            </ul>
+          )}
         </nav>
       </header>
     </>
